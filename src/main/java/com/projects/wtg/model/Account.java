52d6e01ee5519bfbf1a1f9f1901f09c7f5e9c687
jeannascimento <jeanlucas.dev@gmail.com>
2025-09-17 23:33:10 -0300

    Refatoramento de código

--- src/main/java/com/projects/wtg/model/Account.java
+++ src/main/java/com/projects/wtg/model/Account.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
